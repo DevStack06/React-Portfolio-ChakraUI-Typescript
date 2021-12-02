c9a73d66b2f7d0552cefc07619c35375f116ccae
Balram Rathore <balram0698@gmail.com>
2021-12-02 23:22:02 +0530

    Added paraggraph component

--- src/theme.ts
+++ src/theme.ts
@@ -40,6 +40,40 @@ const customTheme = extendTheme({
         },
       },
     },
+    Text: {
+      variants: {
+        "1": {
+          fontSize: "16px",
+          lineHeight: "23px",
+          color: "darkColor",
+        },
+        "2": {
+          fontSize: "22px",
+          lineHeight: "60px",
+          color: "darkColor",
+        },
+        "3": {
+          fontSize: "18px",
+          lineHeight: "26px",
+          color: "darkColor",
+        },
+        "4": {
+          fontSize: "20px",
+          lineHeight: "29.3px",
+          color: "lightColor",
+        },
+        "5": {
+          fontSize: "14px",
+          lineHeight: "20.5px",
+          color: "darkColor",
+        },
+        "6": {
+          fontSize: "20px",
+          lineHeight: "29.38px",
+          color: "darkColor",
+        },
+      },
+    },
   },
 });
 
